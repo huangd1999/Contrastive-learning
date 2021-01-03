c8f58f24973a1629eddf7bc96fae76205c2270ac
Hdong179 <1791683354@qq.com>
2021-01-03 22:53:09 +0800

    implement NCE with lemniscate

---
+++ NCE/aliasmethod.py
@@ -0,0 +1,45 @@
+import torch
+import numpy as np
+class aliasmethod(object):
+    def __init__(self,probs):
+        if probs.sum()>1:
+            probs.div_(probs.sum())
+        K = len(probs)
+        self.prob = torch.zeros(K)
+        self.alias = torch.LongTensor([0]*K)
+        smaller = []
+        larger = []
+        for kk,prob in enumerate(probs):
+            self.prob[kk] = probs[kk] * K
+            if self.prob[kk] < 1:
+                smaller.append(kk)
+            else:
+                larger.append(kk)
+        while len(smaller) > 0 and len(larger) >0:
+            small = smaller.pop()
+            large = larger.pop()
+            self.alias[small] = large
+            self.prob[large] = self.prob[large] - (1 - self.prob[small])
+            if self.prob[large] < 1:
+                smaller.append(large)
+            else:
+                larger.append(large)
+        for last_one in smaller+larger:
+            self.prob[last_one] = 1
+    def cuda(self):
+        self.prob = self.prob.cuda()
+        self.alias = self.alias.cuda()
+    def sample(self,N):
+        '''
+
+        :param N: draw N sample from multinomial
+        :return:
+        '''
+        K = self.alias.size(0)
+        kk = torch.zeros(N, dtype=torch.long, device=self.prob.device).random_(0, K)
+        prob = self.prob.index_select(0,kk)
+        alias = self.alias.index_select(0,kk)
+        b = torch.bernoulli(prob)
+        oq = kk.mul(b.long())
+        oj = alias.mul((1-b).long())
+        return oq+oj
--- NCE/main.py
+++ NCE/main.py
@@ -1,4 +1,7 @@
 import torch
+import sys
+sys.path.append(r'D:\github\Contrastive-learning\NCE')
+import os
 import torchvision
 from torch.autograd import Variable
 from torch import nn
@@ -9,12 +12,103 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader as DataLoader
 from torchvision import datasets
 from torchvision import transforms
-from NCE import NCECriterion
+from NCE import *
 import numpy as np
+
 batch_size = 2
 from PIL import Image
 import torchvision.datasets as datasets
+import Resnet as Resnet
+import torch
+from torch.autograd import Function
+from torch import nn
+from aliasmethod import aliasmethod
+import math
 
+
+class NCEFunction(Function):
+    @staticmethod
+    def forward(self, x, y, memory, idx, params):
+        K = int(params[0].item())
+        T = params[1].item()
+        Z = params[2].item()
+
+        momentum = params[3].item()
+        outputSize = memory.size(0)
+        inputSize = memory.size(1)
+        batchSize = x.size(0)
+
+        # sample positives & negatives
+        idx.select(1, 0).copy_(y.data)
+
+        # sample correspoinding weights
+        weight = torch.index_select(memory, 0, idx.view(-1))
+        weight.resize_(batchSize, K + 1, inputSize)
+
+        # inner product
+        # out = torch.mm(x.data, memory.t())
+        # out.div_(T) # batchSize * N
+        x_change = x.reshape(batchSize, inputSize, 1)
+        out = torch.bmm(weight, x_change)
+        out.div_(T).exp_()  # batchSize * self.K+1
+        x.data.resize_(batchSize, inputSize)
+        if Z < 0:
+            params[2] = out.mean() * outputSize
+            Z = params[2].item()
+            print("normalization constant Z is set to {:.1f}".format(Z))
+        out.div_(Z).resize_(batchSize, K + 1)
+
+        self.save_for_backward(x, memory, y, weight, out, params)
+
+        return out
+
+    @staticmethod
+    def backward(self, gradOutput):
+        x, memory, y, weight, out, params = self.saved_tensors
+        K = int(params[0].item())
+        T = params[1].item()
+        Z = params[2].item()
+        momentum = params[3].item()
+        batchSize = gradOutput.size(0)
+        # gradients d Pm / d linear = exp(linear) / Z
+        gradOutput.data.mul_(out.data)
+        # add temperature
+        gradOutput.data.div_(T)
+        gradOutput.resize_(batchSize, 1, K + 1)
+        # gradient of linear
+        gradInput = torch.bmm(gradOutput, weight)
+        gradInput.resize_as_(x)
+        # update the non-parametric data
+        # weight_pos = memory.index_select(0, y.data.view(-1)).resize_as_(x)
+        weight_pos = weight.select(1, 0).resize_as_(x)
+        weight_pos.mul_(momentum)
+        weight_pos.add_(torch.mul(x.data, 1 - momentum))
+        w_norm = weight_pos.pow(2).sum(1, keepdim=True).pow(0.5)
+        updated_weight = weight_pos.div(w_norm)
+        memory.index_copy_(0, y, updated_weight)
+
+        return gradInput, None, None, None, None
+
+
+class NCEaverage(nn.Module):
+
+    def __init__(self, inputSize, outputSize, K, T=0.07, momentum=0.9, Z=None):
+        super(NCEaverage, self).__init__()
+        self.nLem = outputSize
+        self.unigrams = torch.ones(self.nLem)
+        self.multinomial = aliasmethod(self.unigrams)
+        self.multinomial.cuda()
+        self.K = K
+
+        self.register_buffer('params', torch.tensor([K, T, -1, momentum]));
+        stdv = 1. / math.sqrt(inputSize / 3)
+        self.register_buffer('memory', torch.rand(outputSize, inputSize).mul_(2 * stdv).add_(-stdv))
+
+    def forward(self, x, y):
+        batchSize = x.size(0)
+        idx = self.multinomial.sample(batchSize * (self.K + 1)).view(batchSize, -1)
+        out = NCEFunction.apply(x, y, self.memory, idx, self.params)
+        return out
 class MNISTInstance(datasets.MNIST):
     """MNIST Instance Dataset.
     """
@@ -68,53 +162,17 @@ test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffl
 
 cuda = True if torch.cuda.is_available() else False
 ndata = trainset.__len__()
-class ResidualBlock(nn.Module):
-    def __init__(self,channels):
-        super(ResidualBlock,self).__init__()
-        self.channels = channels
-        self.conv1 = nn.Conv2d(channels,channels,kernel_size=3,padding=1)
-        self.conv2 = nn.Conv2d(channels,channels,kernel_size=3,padding=1)
-    def forward(self,x):
-        y = F.relu(self.conv1(x))
-        y = F.relu(self.conv2(x))
-        return F.relu(x+y)
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net,self).__init__()
-        self.conv1 = nn.Conv2d(1,10,kernel_size=3)
-        self.conv2 = nn.Conv2d(10,100,kernel_size=3)
-        self.conv3 = nn.Conv2d(100,1000,kernel_size=3,padding=1)
-        self.conv4 = nn.Conv2d(1000,10000,kernel_size=3,padding=1)
 
-        self.mp = nn.MaxPool2d(2)
-        self.rblock1 = ResidualBlock(10)
-        self.rblock2 = ResidualBlock(100)
-        self.rblock3 = ResidualBlock(1000)
-        self.rblock4 = ResidualBlock(10000)
-
-        self.fc = nn.Linear(10000,60000)
-
-    def forward(self, x):
-        in_size = x.size(0)
-        x = self.mp(F.relu(self.conv1(x)))
-        x = self.rblock1(x)
-        x = self.mp(F.relu(self.conv2(x)))
-        x = self.rblock2(x)
-        x = self.mp(F.relu(self.conv3(x)))
-        x = self.rblock3(x)
-        x = self.mp(F.relu(self.conv4(x)))
-        x = self.rblock4(x)
-
-        x = x.view(in_size,-1)
-        x = self.fc(x)
-        return x
-model =Net().cuda()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = Resnet.ResNet50().cuda()
 x_list = []
 y_list = []
 criterion = NCECriterion(ndata)
 x = 0
-
+lemniscate = NCEaverage(inputSize= 128, outputSize = ndata, K = 1, T = 0.07, momentum = 0.5)
+lemniscate.to(device)
+criterion.to(device)
 def train(epoch,model,train_loader,optimier,criterion):
     global x
     running_loss = 0.0
@@ -123,11 +181,12 @@ def train(epoch,model,train_loader,optimier,criterion):
         inputs = inputs.cuda()
         index = index.cuda()
         optimier.zero_grad()
+        features = model(inputs)
+        outputs = lemniscate(features, index)
+        loss = criterion(outputs, index)
 
-        #forward,backward,updata
-        outputs = model(inputs)
-        print(outputs)
-        loss = criterion(outputs,index)
+        #print(loss.requires_grad)
+
         loss.backward()
         optimier.step()
         running_loss += loss.item()
@@ -147,3 +206,5 @@ if __name__ =='__main__':
     plt.xlabel('epoch_train')
     plt.ylabel('loss_train')
     plt.show()
+
+
