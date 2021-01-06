07857332905d1bd18c08cb9faeafeec8d24aa955
huangd1999 <huangdong@hust.edu.cn>
2021-01-06 22:20:47 +0800

    change learning rate

--- NCE/main.py
+++ NCE/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 sys.path.append(r'D:\github\Contrastive-learning\NCE')
 import torch.optim as optim
@@ -5,84 +6,167 @@ from torch.utils.data import DataLoader as DataLoader
 from torchvision import transforms
 from NCE import *
 from test import kNN
-batch_size = 128
+from AverageMeter import *
+import time
 import Resnet as Resnet
 import torch
-import random
 from NCEAverage import NCEAverage
 from MNISTInstance import MNISTInstance
+import argparse
+import torch.backends.cudnn as cudnn
 
-lr = 10 ** random.uniform(-5, -2)   # From 1e-5 to 1e-1
-momentum = 1 - 10 ** random.uniform(-3, -1)   # From 0.9 to 0.999
+batch_size = 128
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.03, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', default='', type=str, help='resume from checkpoint')
+parser.add_argument('--test-only', action='store_true', help='test only')
+parser.add_argument('--low-dim', default=128, type=int,
+                    metavar='D', help='feature dimension')
+parser.add_argument('--nce-k', default=4096, type=int,
+                    metavar='K', help='number of negative samples for NCE')
+parser.add_argument('--nce-t', default=0.1, type=float,
+                    metavar='T', help='temperature parameter for softmax')
+parser.add_argument('--nce-m', default=0.5, type=float,
+                    metavar='M', help='momentum for non-parametric updates')
 
-def adjust_learning_rate(optimier, epoch):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr  = 0.1
-    if epoch >= 20:
-        lr = lr * (0.1 ** ((epoch-80) // 40))
-    for param_group in optimier.param_groups:
-        param_group['lr'] = lr
+args = parser.parse_args()
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
+# Data
+print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.RandomResizedCrop(size=28, scale=(0.2,1.)),
+    transforms.RandomResizedCrop(size=32, scale=(0.2, 1.)),
     transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
     transforms.RandomGrayscale(p=0.2),
-    transforms.RandomHorizontalFlip(),
+    # transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.1307,),(0.3081,)),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.1307,),(0.3081,)),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 trainset = MNISTInstance(root='../data/mnist', train=True, download=True, transform=transform_train)
 
 testset = MNISTInstance(root='../data/mnist', train=False, download=True, transform=transform_test)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
-train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
-test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
-
-cuda = True if torch.cuda.is_available() else False
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 ndata = trainset.__len__()
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = Resnet.ResNet50().cuda()
-criterion = NCECriterion(ndata)
-x = 0
-lemniscate = NCEAverage(inputSize= 10, outputSize = ndata, K = 10, T = 0.07, momentum = momentum)
+
+print('==> Building model..')
+net = Resnet.ResNet18().cuda()
+# define leminiscate
+
+lemniscate = NCEAverage(args.low_dim, ndata, args.nce_k, args.nce_t, args.nce_m)
+
+
+if device == 'cuda':
+    net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
+    cudnn.benchmark = True
+
+# Model
+if args.test_only or len(args.resume) > 0:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/' + args.resume)
+    net.load_state_dict(checkpoint['net'])
+    lemniscate = checkpoint['lemniscate']
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
+
+# define loss function
+if hasattr(lemniscate, 'K'):
+    criterion = NCECriterion(ndata)
+else:
+    criterion = nn.CrossEntropyLoss()
+
+net.to(device)
 lemniscate.to(device)
 criterion.to(device)
-def train(epoch,model,train_loader,optimier,criterion):
-    global x
-    adjust_learning_rate(optimier, epoch)
-    running_loss = 0.0
-    epoch_i = 0
-    for i,(inputs, _, index) in enumerate(train_loader, 0):
-        inputs = inputs.cuda()
-        index = index.cuda()
-        optimier.zero_grad()
-        features = model(inputs)
-        outputs = lemniscate(features, index)
-        loss = criterion(outputs)
 
-        #print(loss.requires_grad)
+if args.test_only:
+    acc = kNN(0, net, lemniscate, trainloader, testloader, 200, args.nce_t, 1)
+    sys.exit(0)
+
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.lr
+    if epoch >= 80:
+        lr = args.lr * (0.1 ** ((epoch - 80) // 40))
+    print(lr)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    adjust_learning_rate(optimizer, epoch)
+    train_loss = AverageMeter()
+    data_time = AverageMeter()
+    batch_time = AverageMeter()
+    correct = 0
+    total = 0
+
+    # switch to train mode
+    net.train()
+
+    end = time.time()
+    for batch_idx, (inputs, targets, indexes) in enumerate(trainloader):
+        data_time.update(time.time() - end)
+        inputs, targets, indexes = inputs.to(device), targets.to(device), indexes.to(device)
+        optimizer.zero_grad()
+
+        features = net(inputs)
+        outputs = lemniscate(features, indexes)
+        loss = criterion(outputs, indexes)
 
         loss.backward()
-        optimier.step()
-        running_loss += loss.item()
-        epoch_i =epoch_i+1
-        x += 1
-        if i %100 ==99:
-            print('[%d,%5d]loss:%.3f'%(epoch+1,i+1,running_loss/100))
-            running_loss = 0.0
+        optimizer.step()
 
-optimier = optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=5e-4)
-if __name__ =='__main__':
-    epoch_i = 0
-    for epoch in range(200):
-        train(epoch,model,train_loader,optimier,criterion)
-        kNN(epoch, model, lemniscate, train_loader, test_loader, 200, 0.07)
+        train_loss.update(loss.item(), inputs.size(0))
 
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        print('Epoch: [{}][{}/{}]'
+              'Time: {batch_time.val:.3f} ({batch_time.avg:.3f}) '
+              'Data: {data_time.val:.3f} ({data_time.avg:.3f}) '
+              'Loss: {train_loss.val:.4f} ({train_loss.avg:.4f})'.format(
+            epoch, batch_idx, len(trainloader), batch_time=batch_time, data_time=data_time, train_loss=train_loss))
+
+
+for epoch in range(start_epoch, start_epoch + 200):
+    train(epoch)
+    acc = kNN(epoch, net, lemniscate, trainloader, testloader, 200, args.nce_t, 0)
+
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.state_dict(),
+            'lemniscate': lemniscate,
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt.t7')
+        best_acc = acc
+
+    print('best accuracy: {:.2f}'.format(best_acc * 100))
+
+acc = kNN(0, net, lemniscate, trainloader, testloader, 200, args.nce_t, 1)
+print('last accuracy: {:.2f}'.format(acc * 100))
---
+++ NCE/old.py
@@ -0,0 +1,87 @@
+import sys
+sys.path.append(r'D:\github\Contrastive-learning\NCE')
+import torch.optim as optim
+from torch.utils.data import DataLoader as DataLoader
+from torchvision import transforms
+from NCE import *
+from test import kNN
+batch_size = 128
+import Resnet as Resnet
+import torch
+import random
+from NCEAverage import NCEAverage
+from MNISTInstance import MNISTInstance
+
+lr = 10 ** random.uniform(-5, -2)   # From 1e-5 to 1e-1
+momentum = 1 - 10 ** random.uniform(-3, -1)   # From 0.9 to 0.999
+
+def adjust_learning_rate(optimier, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr  = 0.1
+    if epoch >= 20:
+        lr = lr * (0.1 ** ((epoch-80) // 40))
+    for param_group in optimier.param_groups:
+        param_group['lr'] = lr
+
+
+transform_train = transforms.Compose([
+    transforms.RandomResizedCrop(size=28, scale=(0.2,1.)),
+    transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
+    transforms.RandomGrayscale(p=0.2),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,),(0.3081,)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,),(0.3081,)),
+])
+
+trainset = MNISTInstance(root='../data/mnist', train=True, download=True, transform=transform_train)
+
+testset = MNISTInstance(root='../data/mnist', train=False, download=True, transform=transform_test)
+
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+cuda = True if torch.cuda.is_available() else False
+ndata = trainset.__len__()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = Resnet.ResNet50().cuda()
+criterion = NCECriterion(ndata)
+x = 0
+lemniscate = NCEAverage(inputSize= 10, outputSize = ndata, K = 10, T = 0.07, momentum = momentum)
+lemniscate.to(device)
+criterion.to(device)
+def train(epoch,model,train_loader,optimier,criterion):
+    global x
+    adjust_learning_rate(optimier, epoch)
+    running_loss = 0.0
+    epoch_i = 0
+    for i,(inputs, _, index) in enumerate(train_loader, 0):
+        inputs = inputs.cuda()
+        index = index.cuda()
+        optimier.zero_grad()
+        features = model(inputs)
+        outputs = lemniscate(features, index)
+        loss = criterion(outputs)
+
+        #print(loss.requires_grad)
+
+        loss.backward()
+        optimier.step()
+        running_loss += loss.item()
+        epoch_i =epoch_i+1
+        x += 1
+        if i %100 ==99:
+            print('[%d,%5d]loss:%.3f'%(epoch+1,i+1,running_loss/100))
+            running_loss = 0.0
+
+optimier = optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=5e-4)
+if __name__ =='__main__':
+    epoch_i = 0
+    for epoch in range(200):
+        train(epoch,model,train_loader,optimier,criterion)
+        kNN(epoch, model, lemniscate, train_loader, test_loader, 200, 0.07)
