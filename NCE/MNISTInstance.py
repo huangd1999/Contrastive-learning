bcdb1ecc0043c60573edde7de50c450537b5f6df
huangd1999 <huangdong@hust.edu.cn>
2021-01-06 22:30:31 +0800

    change learning rate

--- NCE/MNISTInstance.py
+++ NCE/MNISTInstance.py
@@ -1,13 +1,15 @@
 import torchvision.datasets as datasets
 from PIL import Image
+
+
 class CIFAR10Instance(datasets.CIFAR10):
     """CIFAR10Instance Dataset.
     """
     def __getitem__(self, index):
         if self.train:
-            img, target = self.train_data[index], self.train_labels[index]
+            img, target = self.data[index], self.targets[index]
         else:
-            img, target = self.test_data[index], self.test_labels[index]
+            img, target = self.data[index], self.targets[index]
 
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
@@ -20,19 +22,3 @@ class CIFAR10Instance(datasets.CIFAR10):
             target = self.target_transform(target)
 
         return img, target, index
-
-class CIFAR100Instance(CIFAR10Instance):
-    """CIFAR100Instance Dataset.
-    This is a subclass of the `CIFAR10Instance` Dataset.
-    """
-    base_folder = 'cifar-100-python'
-    url = "https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz"
-    filename = "cifar-100-python.tar.gz"
-    tgz_md5 = 'eb9058c3a382ffc7106e4002c42a8d85'
-    train_list = [
-        ['train', '16019d7e3df5f24257cddd939b257f8d'],
-    ]
-
-    test_list = [
-        ['test', 'f0ef6b0ae62326f3e7ffdfab6717acfc'],
-    ]
